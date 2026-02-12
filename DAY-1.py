4147f33f40c232cfce745bb71ac33af2525bd1ac
MahiAdithya2003 <mahiaditya666@gmail.com>
2026-02-12 11:08:37 +0530

    Add files via upload
    Day-1 : Introduction to python.

---
+++ DAY-1.py
@@ -0,0 +1,4 @@
+print("his is Adithya first day in the codegynan")
+print("today i have learned Introduction about python")
+print("python is easy to learn and implement")
+print("python is a high level language")
