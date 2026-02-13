2cf1d43596177f51e22625248e1d87ebc3575666
MahiAdithya2003 <mahiaditya666@gmail.com>
2026-02-13 11:00:49 +0530

    Add files via upload
    day-2 in Codegnan today I learned about  IO operations and type conversion

---
+++ day-2( IO operations and type conversion).py
@@ -0,0 +1,33 @@
+"""#read input numbers for user
+num=input()
+print(type(num))#checking the type of number
+print(num)"""
+
+
+
+
+""" #implict type conversion
+
+
+num1=int(input())
+val=float(input())
+print(type(num1),type(val))#checking the type of number
+res=num1+val
+print(res)"""
+
+
+"""num1=input()#read a string type numbers
+print(type(num1))#checking the type of number
+res=num1+10
+print(res)"""
+
+"""#to over the above problem using the explict type conversion
+num1=input()
+print(type(num1))
+res=int(num1)+10
+print(res)"""
+
+
+#converting integer into float and string&bollean
+num="hi"
+print(bool(num))
