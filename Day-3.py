7dcd5b4b88f1195ce0bf19d190e728247ccdd989
MahiAdithya2003 <mahiaditya666@gmail.com>
2026-02-14 14:01:45 +0530

    Add files via upload
    Day-3 Types of operations

---
+++ Day-3.py
@@ -0,0 +1,28 @@
+"""num="12.5"
+print(int(float(num)))"""
+
+"""#check number is zero
+num=int(input())
+#
+if num==0:
+    print("number is zero")
+print("program done")"""
+"""#check number is zero or not
+num=int(input())
+if num==0:
+    print("number is zero")
+else:
+     print("number is not zero")
+print("program done")"""
+#read to number which one is bigger one
+num1=10
+num2=15
+if(num1>num2):
+    print("num1")
+else:
+    print("num2 is bigger")
+   
+
+    
+    
+ 
