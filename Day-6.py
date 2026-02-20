6a5f50f1028ffb08e2a7579ba9eea4976c24b540
MahiAdithya2003 <mahiaditya666@gmail.com>
2026-02-20 21:40:42 +0530

    Add files via upload

---
+++ Day-6.py
@@ -0,0 +1,62 @@
+"""num=200
+while num>=1:
+    print(num, end=" ")
+    num-=1"""
+
+n1m=(int,input().spilt())
+total=0
+while n<m:
+    if n%2==0:
+        total+=n
+        n+=1
+primt(total)
+
+
+"""n1m=int,input().spilt())
+total=0
+if n%2!=0:
+    n=n+1
+    while n<m:
+        if n%2==0:
+            total+=n
+            n+=2
+    print(num)"""
+
+
+#find the length of number
+#find sum of the digits in a number
+#reverse of a number
+#check weather the number is palindrome or not
+#check weather  the number is armstrong
+
+
+"""
+n=156
+count=0
+while n>0:
+    n//=10
+    count+=1
+print(count)"""
+
+
+"""n=156
+s=0
+while n>0:
+    rem=n%10
+    n=n//10
+    s+=rem
+print(s)"""
+
+"""n=123
+rev=0
+while n>0:
+    rem=n%10
+    rev=rev*10+rem
+    n//=10
+print(rev)"""
+
+
+
+
+
+
